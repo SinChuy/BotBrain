77ee7fd8593d17e27bcce302052dc1e37a34bc3a
Hey-Zeus <JLopezCera@gmail.com>
2023-03-16 17:27:38 +0000

    Disabled Pylint and moved order of methods in twitch_bot.py

--- Code/twitch_bot.py
+++ Code/twitch_bot.py
@@ -1,11 +1,12 @@
 import os
 import openai
 from twitchio.ext import commands
+import requests
 
 # Set up OpenAI API key
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-str token1 = os.environ['TMI_TOKEN']
+token1 = os.environ['TMI_TOKEN']
 
 
 # Define the bot class
@@ -19,18 +20,7 @@ class TwitchBot(commands.Bot):
                          initial_channels=[os.environ['CHANNEL_NAME']])
 
         self.loop.run_until_complete(self.start())
-
-    async def event_ready(self):
-        print(f'{os.environ["BOT_NICK"]} has connected to Twitch!')
-
-    async def event_message(self, message):
-        # Ignore messages from the bot itself
-        if message.author.name.lower() == os.environ['BOT_NICK'].lower():
-            return
-
-        # Process commands
-        await self.handle_commands(message)
-
+    
     @commands.command(name='ask')
     async def ask_command(self, ctx, *, question):
         # Generate a response using ChatGPT
@@ -50,7 +40,42 @@ class TwitchBot(commands.Bot):
         )
 
         return response.choices[0].text.strip()
-
+    
     async def send_message(self, message):
         channel = self.get_channel(os.environ['CHANNEL_NAME'])
         await channel.send(message)
+        
+    async def event_ready(self):
+        print(f'{os.environ["BOT_NICK"]} has connected to Twitch!')
+    
+    async def create_clip(self, client_id, oauth_token, broadcaster_id):
+        headers = {
+            "Client-ID": client_id,
+            "Authorization": f"Bearer {oauth_token}"
+        }
+        url = f"https://api.twitch.tv/helix/clips?broadcaster_id={broadcaster_id}"
+        response = requests.post(url, headers=headers)
+
+        if response.status_code == 202:
+            clip_data = response.json()
+            return clip_data["data"][0]["edit_url"]
+        else:
+            return None
+        
+    async def event_message(self, message):
+        # Ignore messages from the bot itself
+        clip_url = ""
+        if message.author.name == os.environ['BOT_NICK'].lower():
+            return
+        
+        if message.content.lower() == "!clip":
+            clip_url = await self.create_clip(self, os.environ['CLIENT_ID'], os.environ['TMI_TOKEN'], '217378869')
+            if clip_url:
+                await message.channel.send(f"Here's a clip of the stream: {clip_url}")
+            else:
+                await message.channel.send("Failed to create a clip. Please try again later.")
+            # Process commands
+
+        await self.handle_commands(message)
+
+  
